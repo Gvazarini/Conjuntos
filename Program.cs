22c6489a2e1e207360e4227ac87bb9ff19fd9665
Gabriel Vazarini <camaravazarini@gmail.com>
2022-06-17 04:56:52 -0300

    Conjunto

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Conjuntos.Entities;
+namespace Conjuntos
+{
+    class Progrma
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            Console.Write("Enter fille path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while ( !sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { UserName = name, Instant = instant });
+                    }
+                    Console.Write("Total users: " + set.Count);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
